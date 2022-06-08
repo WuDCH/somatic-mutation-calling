d5a0818f329413f0e149453e293b440eb5a1f27a
Chih Hao Wu <105507102+WuDCH@users.noreply.github.com>
2022-06-08 01:45:07 -0400

    Update generate-batch-scripts.py

--- generate-batch-scripts.py
+++ generate-batch-scripts.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import argparse
 import yaml
+import re
 
 # Match tumour and normal sample accession codes based on sample name
 
@@ -46,41 +47,40 @@ class WESSample:
 
     # TODO change to switch/match case so can modify run to run.sra or prefix{run}.sra easily
     # maybe change parameter sra_suffix to accept both prefix and suffix
-    def batch_reads_retrieval(self, scripts_directory: str, write_directory: str, sra_suffix = "", cmd="#!/bin/bash\n"):
+    def batch_reads_retrieval(self, scripts_directory: str, write_directory: str, prj_filename = "", dbgap_filename = "?", cmd="#!/bin/bash\n"):
         # TODO start from sra file, for restricted datasets
         cmd += "module load sratoolkit\n"
         for run in self.all_runs:
-            run = run if sra_suffix == "" else f"{write_directory}/{run}{sra_suffix}.sra"
-            cmd += (f"bash {scripts_directory}/fasterq-dump.sh {run} {write_directory}\n")
+            run = re.sub('\?', run, dbgap_filename)
+            # print(run)
+            # run if sra_suffix == "" else f"{write_directory}/{run}{sra_suffix}.sra"
+            cmd += (f"bash {scripts_directory}/fasterq-dump-from-dbGaP.sh {run} {write_directory} {prj_filename}\n")
         WESSample.write_to_file(cmd.rstrip(), f"{name}-get-reads.sh")
 
-    def batch_reads_quality_control(self, scripts_directory: str, raw_fastq_directory: str, write_directory: str, sra_suffix = "", cmd="#!/bin/bash\n"):
+    def batch_reads_quality_control(self, scripts_directory: str, raw_fastq_directory: str, write_directory: str, cmd="#!/bin/bash\n"):
         cmd += "module load fastqc trimgalore\n"
         for run in self.all_runs:
-            run = run if sra_suffix == "" else f"{run}{sra_suffix}" # HERE
-            cmd += (f"bash {scripts_directory}/fastq-qc.sh {raw_fastq_directory}/{run}_1.fastq "
+            cmd += (f"bash {scripts_directory}/fastq-quality-control.sh {raw_fastq_directory}/{run}_1.fastq "
                 f"{raw_fastq_directory}/{run}_1.fastq "
                 f"{raw_fastq_directory}/{run}_2.fastq "
                 f"{write_directory}\n")
         WESSample.write_to_file(cmd.rstrip(), f"{name}-quality-control-reads.sh")
 
-    def batch_reads_mapping(self, scripts_directory: str, trimmed_fastq_directory: str, write_directory: str, sra_suffix = "", cmd="#!/bin/bash\n"):
+    def batch_reads_mapping(self, scripts_directory: str, trimmed_fastq_directory: str, write_directory: str, cmd="#!/bin/bash\n"):
         cmd += "module load bwa samtools\n"
         for run in self.all_runs:
-            run = run if sra_suffix == "" else f"{run}{sra_suffix}" # HERE
             cmd += (f"{scripts_directory}/fastq-read-alignment.sh {trimmed_fastq_directory}/{run}_1_val_1.fq "
                 f"{trimmed_fastq_directory}/{run}_2_val_2.fq {write_directory} {run}.bam\n")
         WESSample.write_to_file(cmd.rstrip(), f"{name}-mapping-reads.sh")
 
-    def batch_bams_preprocessing(self, scripts_directory: str, mapped_reads_directory: str, write_directory: str, intervals_filename: str, sra_suffix = "", cmd="#!/bin/bash\n"):
+    def batch_bams_preprocessing(self, scripts_directory: str, mapped_reads_directory: str, write_directory: str, intervals_filename: str, cmd="#!/bin/bash\n"):
         cmd += "module load GATK samtools\n"
         for run in self.all_runs:
-            run = run if sra_suffix == "" else f"{run}{sra_suffix}" # HERE
             cmd += (f"bash {scripts_directory}/bam-process.sh {mapped_reads_directory}/{run}.bam {run}_rg "
                 f"{write_directory} {run} {intervals_filename}\n")
         WESSample.write_to_file(cmd.rstrip(), f"{name}-preprocessing-bams.sh")
 
-    def swarm_vcfs_mutation_calling(self, processed_bams_directory: str, write_directory: str, sra_suffix = "", cmd=""):
+    def swarm_vcfs_mutation_calling(self, processed_bams_directory: str, write_directory: str, cmd=""):
         _num = list(range(1,22+1))+['X','Y','M']
         chr = ["chr"+str(n) for n in _num]
 
@@ -89,10 +89,6 @@ class WESSample:
             normal_sample = runs["normal"][0]
             tumour_sample = runs["tumour"][0]
             sample_name = sample.replace(" ","")
-
-            # HERE 
-            normal_sample = normal_sample if sra_suffix == "" else f"{normal_sample}{sra_suffix}"
-            tumour_sample = tumour_sample if sra_suffix == "" else f"{tumour_sample}{sra_suffix}"
 
             for chr_i in chr:
                 cmd += ("module load GATK; gatk Mutect2 "
@@ -109,7 +105,7 @@ class WESSample:
 
             cmd = "" # clear string
             
-    def batch_vcfs_annotations(self, processed_bams_directory: str, vcf_directory: str, write_directory: str, intervals_filename: str, sra_suffix = "", cmd=""):
+    def batch_vcfs_annotations(self, processed_bams_directory: str, vcf_directory: str, write_directory: str, intervals_filename: str, cmd=""):
         _num = list(range(1,22+1))+['X','Y','M']
         cmd += "module load GATK picard\n"
 
@@ -118,10 +114,6 @@ class WESSample:
             normal_sample = runs["normal"][0] 
             tumour_sample = runs["tumour"][0]
             sample_name = sample.replace(" ","")
-
-            # HERE 
-            normal_sample = normal_sample if sra_suffix == "" else f"{normal_sample}{sra_suffix}"
-            tumour_sample = tumour_sample if sra_suffix == "" else f"{tumour_sample}{sra_suffix}"
 
             vcf_per_chr = " ".join([f"I={vcf_directory}/{sample_name}_chr{str(n)}.vcf" for n in _num])
             cmd += f"java -Xmx16g -jar $PICARDJAR MergeVcfs {vcf_per_chr} O={write_directory}/merged_{sample_name}.vcf\n"
@@ -187,11 +179,11 @@ if __name__ == "__main__":
     example = WESSample(name, metadata_filename, (kw_normal, kw_tumour))
     example.preprocess_runinfo()
     example.groupby_sample()
-    example.batch_reads_retrieval(scripts, raw_reads, sra_suffix="_dbGaP-25281")
-    example.batch_reads_quality_control(scripts, raw_reads, trimmed_reads, sra_suffix="_dbGaP-25281")
-    example.batch_reads_mapping(scripts, trimmed_reads, aligned_reads, sra_suffix="_dbGaP-25281")
-    example.batch_bams_preprocessing(scripts, aligned_reads, processed_bams, intervals_filename, sra_suffix="_dbGaP-25281")
-    example.swarm_vcfs_mutation_calling(processed_bams, mutation_vcf, sra_suffix="_dbGaP-25281")
-    example.batch_vcfs_annotations(processed_bams, mutation_vcf, mutation_vcf, intervals_filename, sra_suffix="_dbGaP-25281")
+    example.batch_reads_retrieval(scripts, raw_reads, prj_filename="~/prj_25281.ngc", dbgap_filename="Run_?/?_dbGaP-25281.sra")
+    example.batch_reads_quality_control(scripts, raw_reads, trimmed_reads)
+    example.batch_reads_mapping(scripts, trimmed_reads, aligned_reads)
+    example.batch_bams_preprocessing(scripts, aligned_reads, processed_bams, intervals_filename)
+    example.swarm_vcfs_mutation_calling(processed_bams, mutation_vcf)
+    example.batch_vcfs_annotations(processed_bams, mutation_vcf, mutation_vcf, intervals_filename)
 
     # TODO organize output files
