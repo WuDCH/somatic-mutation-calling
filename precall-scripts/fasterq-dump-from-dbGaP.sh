423ac1dded6d9afeb1db6158747ed1b84dc9d135
Chih Hao Wu <105507102+WuDCH@users.noreply.github.com>
2022-06-07 21:00:07 -0400

    Update fasterq-dump-from-dbGaP.sh

--- precall-scripts/fasterq-dump-from-dbGaP.sh
+++ precall-scripts/fasterq-dump-from-dbGaP.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+: <<'END'
+Retrieve the raw fastq files for paired-end read experiments
+argument(s): SRA run accession
+output(s): 2 fastq files containing the reads
+END
+
+SRR_ACCESSION=$1
+OUT_DIR=$2
+NGC_FILE=$3
+
+fasterq-dump --ngc ${NGC_FILE} -t /lscratch/$SLURM_JOBID -O ${OUT_DIR} ${SRR_ACCESSION}
